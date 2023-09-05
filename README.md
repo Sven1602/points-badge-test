d7e5d70bcf557159926997f9d3ae6a01a9babb1c
Sven1602 <hartmann.sven81@web.de>
2023-09-05 13:08:32 +0200

    # Methode als async deklariert und die Tests angepasst

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# points-badge-test" Task 2
+# points-badge-test
+
+Please clone this repo into your account and send commits or branches with your results to [Simon](mailto:s.hansen@digital-h.de).
+
+## Task I 
+
+Please refactor the function `getUsersBadge()`. You can provide multiple version. Please describe in the commit messages
+what has improved in your version.
+
+## Task II
+
+Please make the function `getUsersBadge` async.
+
+Please check branch task-3 for the next assignment.
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
-export const getUsersBadge = ( user: User ): Icon | null => {
-  let badge = null;
+export async function getUsersBadge(user: User): Promise<Icon | null> {
+  
+  let badge: Icon | null = null;
   switch ( true ) {
     case ( user.solution_count >= 5 && user.solution_count < 25 ):
       badge = Icon.BADGE_BRONZE;
@@ -15,4 +16,4 @@ export const getUsersBadge = ( user: User ): Icon | null => {
       break;
   }
   return badge;
-};
+}
